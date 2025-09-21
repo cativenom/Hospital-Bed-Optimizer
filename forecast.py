1547df5e4ff9b452c2367c9cf8900150c80b4c9c
thxliu <thxliu@smith.edu>
2025-09-20 20:48:31 -0400

    updated sample data

--- forecast.py
+++ forecast.py
@@ -6,11 +6,43 @@ df = pd.read_csv('sample_data.csv')
 # convert admission date column to type datetime
 df['Date of Admission'] = pd.to_datetime(df['Date of Admission'])
 
-# eename columns to Prophet format
+# rename columns to Prophet format
 df = df.rename(columns={
     'Date of Admission': 'ds',
-    'Expected Admission Length': 'y'
-})
+    'Count': 'y'
+    })
+
+# calculate net changes: +1 if patient admitted, -1 if patient discharged
+df["net"] = df.apply(
+    "net" row: row["Count"] if row["Type"].lower() == "admission" else -row["Count"],
+    axis=1
+    )
+
+# aggregate net changes per type per day
+daily_net_changes = df.groupby(["ds", "Type"])["net"].sum().reset_index()
+
+# starts with a base level occupancy (starting number of patients occupying a bed on any given day without
+# having made changes yet) and simulates running total of patients for each admission type
+
+base_occupancy = 20
+types_forecast = []
+
+for type in daily_net_changes['Admission Type'].unique():
+    # gives us the time series of net admissions/discharges for a specific admission type
+    type_df = daily_net_changes[daily_net_changes['Admission Type'] == type].copy()
+
+    # ensures that there’s one row for every single day, even if there are no events on that day b/c Prophet needs a 
+    # continuous time series
+    type_df = type_df.set_index("ds").asfreq("D", fill_value=0).reset_index()
+
+    # calculates the running total of patients over time by adding the net changes in admitted patients over time to the base occupancy
+    type_df["occupancy"] = type_df["net"].cumsum() + base_occupancy
+
+
+df["admission_type"] = type
+
+
+
 
 # instantiating a new Prophet object
 m = Prophet()
@@ -22,3 +54,4 @@ future.tail()
 
 forecast = m.predict(future)
 print(forecast)
+
