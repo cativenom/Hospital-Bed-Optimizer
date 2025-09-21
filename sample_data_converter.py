165b128f5078b0014bf097b043f7dbdc98cbe478
thxliu <thxliu@smith.edu>
2025-09-20 20:46:39 -0400

    updated sample data

--- sample_data_converter.py
+++ sample_data_converter.py
@@ -1,21 +1,34 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy
-from scipy.stats import poisson
-
-# make an expected admission length
-expected_stay = np.random.poisson(2, 55500)
+import random
 
 # read in file
 f=pd.read_csv("/Users/tanaliu/Downloads/healthcare_dataset.csv")
 
+# # make an expected number of patients admitted or discharged per day
+# expected_count = np.random.poisson(2, 55500)
+
+# status for patients (either admitted or discharged) on a specific date
+types = ["admission", "discharge"]
+
+# generate random data -- randomly assigns a 'type' (admission or discharge) and expected patient count on a date
+patient_status = []
+for i in range(len(f)):
+    row = random.choice(types)
+    patient_status.append(row)
+
+expected_count = []
+
+for i in range(len(f)):
+    row = random.randint(1, 10)
+    expected_count.append(row)
+
 # columns to keep
-keep_col = ['Age','Medical Condition','Date of Admission','Admission Type']
+keep_col = ['Date of Admission','Admission Type']
 new_f = f[keep_col].copy()
 
-# add expected admission length column to the csv file
-new_f['Expected Admission Length'] = expected_stay
+# add patient status and expected patient count to the CSV
+new_f['Patient Status'] = patient_status
+new_f['Count'] = expected_count
 
 # write the dataframe to a csv file
 new_f.to_csv("sample_data.csv", index=False)
